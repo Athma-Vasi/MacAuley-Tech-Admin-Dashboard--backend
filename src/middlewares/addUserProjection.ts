d1ac3957a89888e7dff932390cd90d17d2c7089c
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-03-30 10:03:49 -0700

    add userProjection middleware

---
+++ src/middlewares/addUserProjection.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import { PROPERTY_DESCRIPTOR } from "../constants";
+
+function addUserProjection(
+    request: Request,
+    _response: Response,
+    next: NextFunction,
+) {
+    const projection = ["-password"];
+    Object.defineProperty(request.query, "projection", {
+        value: projection,
+        ...PROPERTY_DESCRIPTOR,
+    });
+
+    return next();
+}
+
+export { addUserProjection };
