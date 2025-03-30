7da0b2f06cf9af89a96ba249daee596eb149c619
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-03-30 10:10:10 -0700

    add auth: registerUserHandler

--- src/resources/auth/handlers.ts
+++ src/resources/auth/handlers.ts
@@ -2,20 +2,26 @@ import { Response } from "express";
 import jwt from "jsonwebtoken";
 import { Model } from "mongoose";
 import { CONFIG } from "../../config";
-import { ACCESS_TOKEN_EXPIRY } from "../../constants";
+import { ACCESS_TOKEN_EXPIRY, HASH_SALT_ROUNDS } from "../../constants";
 import {
     createNewResourceService,
     getResourceByFieldService,
 } from "../../services";
-import { DBRecord, HttpResult, LoginUserRequest } from "../../types";
+import {
+    CreateNewResourceRequest,
+    DBRecord,
+    HttpResult,
+    LoginUserRequest,
+} from "../../types";
 import {
     compareHashedStringWithPlainStringSafe,
     createErrorLogSchema,
     createHttpResultError,
     createHttpResultSuccess,
+    hashStringSafe,
 } from "../../utils";
 import { ErrorLogModel } from "../errorLog";
-import { UserDocument, UserModel } from "../user";
+import { UserDocument, UserModel, UserSchema } from "../user";
 
 // @desc   Login user
 // @route  POST /auth/login
@@ -170,6 +176,116 @@ function loginUserHandler<
                 createHttpResultSuccess({
                     accessToken,
                     data: [userDocPartial],
+                }),
+            );
+        } catch (error: unknown) {
+            await createNewResourceService(
+                createErrorLogSchema(
+                    error,
+                    request.body,
+                ),
+                ErrorLogModel,
+            );
+
+            response.status(200).json(createHttpResultError({}));
+        }
+    };
+}
+
+// @desc   Register user
+// @route  POST /auth/register
+// @access Public
+function registerUserHandler<
+    Doc extends DBRecord = DBRecord,
+>(
+    model: Model<Doc>,
+) {
+    return async (
+        request: CreateNewResourceRequest<UserSchema>,
+        response: Response,
+    ) => {
+        try {
+            const { schema } = request.body;
+            const { username, password } = schema;
+
+            const getUserResult = await getResourceByFieldService({
+                filter: { username },
+                model,
+            });
+
+            if (getUserResult.err) {
+                await createNewResourceService(
+                    createErrorLogSchema(getUserResult.val, request.body),
+                    ErrorLogModel,
+                );
+
+                response.status(200).json(
+                    createHttpResultError({
+                        message: "Unable to register. Please try again.",
+                    }),
+                );
+                return;
+            }
+
+            const unwrappedResult = getUserResult.safeUnwrap();
+
+            if (unwrappedResult.kind === "success") {
+                response.status(200).json(
+                    createHttpResultError({
+                        message: "Username already exists",
+                    }),
+                );
+                return;
+            }
+
+            const hashPasswordResult = await hashStringSafe({
+                saltRounds: HASH_SALT_ROUNDS,
+                stringToHash: password,
+            });
+
+            if (hashPasswordResult.err) {
+                await createNewResourceService(
+                    createErrorLogSchema(hashPasswordResult.val, request.body),
+                    ErrorLogModel,
+                );
+
+                response.status(200).json(
+                    createHttpResultError({
+                        message: "Unable to register. Please try again.",
+                    }),
+                );
+                return;
+            }
+
+            const hashedPassword = hashPasswordResult.safeUnwrap().data;
+            const userSchema = {
+                ...schema,
+                password: hashedPassword,
+            };
+
+            const createUserResult = await createNewResourceService(
+                userSchema,
+                model,
+            );
+
+            if (createUserResult.err) {
+                await createNewResourceService(
+                    createErrorLogSchema(createUserResult.val, request.body),
+                    ErrorLogModel,
+                );
+
+                response.status(200).json(
+                    createHttpResultError({
+                        message: "Unable to register. Please try again.",
+                    }),
+                );
+                return;
+            }
+
+            response.status(200).json(
+                createHttpResultSuccess({
+                    accessToken: "",
+                    message: "User registered successfully",
                 }),
             );
         } catch (error: unknown) {
