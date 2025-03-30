52659469e0ee8f27eb007f1af3a1f347e8b1b4f6
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-03-30 10:30:12 -0700

    add auth: index

---
+++ src/resources/auth/index.ts
@@ -0,0 +1,28 @@
+/**
+ * This barrel file is used to import/export auth model, router, types, handlers and services
+ */
+
+import { authRouter } from "./routes";
+/**
+ * Imports
+ */
+import {
+    loginUserHandler,
+    logoutUserHandler,
+    registerUserHandler,
+} from "./handlers";
+import { type AuthDocument, AuthModel, type AuthSchema } from "./model";
+import { createTokenService } from "./services";
+/**
+ * Exports
+ */
+export {
+    AuthModel,
+    authRouter,
+    createTokenService,
+    loginUserHandler,
+    logoutUserHandler,
+    registerUserHandler,
+};
+
+export type { AuthDocument, AuthSchema };
