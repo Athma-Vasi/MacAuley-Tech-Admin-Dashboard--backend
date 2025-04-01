ae5b278009c4c92f42bdadcc356b06570aa3a368
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-04-01 15:31:25 -0700

    add customer: handlers

---
+++ src/resources/metrics/customer/handlers.ts
@@ -0,0 +1,70 @@
+import type { Response } from "express";
+import type { Model } from "mongoose";
+import { createNewResourceService } from "../../../services";
+import type { CreateNewResourceRequest, DBRecord } from "../../../types";
+import {
+  createErrorLogSchema,
+  createHttpResultError,
+  createHttpResultSuccess,
+} from "../../../utils";
+import { ErrorLogModel } from "../../errorLog";
+import type { CustomerMetricsSchema } from "./model";
+
+// @desc   Create new Customer Metric
+// @route  POST /api/v1/metrics/customer
+// @access Private/Admin/Manager
+function createNewCustomerMetricHandler<
+  Doc extends DBRecord = DBRecord,
+>(
+  model: Model<Doc>,
+) {
+  return async (
+    request: CreateNewResourceRequest<CustomerMetricsSchema>,
+    response: Response,
+  ) => {
+    try {
+      const { schema } = request.body;
+
+      const createCustomerMetricResult = await createNewResourceService(
+        schema,
+        model,
+      );
+
+      if (createCustomerMetricResult.err) {
+        await createNewResourceService(
+          createErrorLogSchema(
+            createCustomerMetricResult.val,
+            request.body,
+          ),
+          ErrorLogModel,
+        );
+
+        response.status(200).json(
+          createHttpResultError({
+            message: "Unable to create customer metric. Please try again.",
+          }),
+        );
+        return;
+      }
+
+      response.status(200).json(
+        createHttpResultSuccess({
+          accessToken: "",
+          message: "Customer Metric created successfully",
+        }),
+      );
+    } catch (error: unknown) {
+      await createNewResourceService(
+        createErrorLogSchema(
+          error,
+          request.body,
+        ),
+        ErrorLogModel,
+      );
+
+      response.status(200).json(createHttpResultError({}));
+    }
+  };
+}
+
+export { createNewCustomerMetricHandler };
