7776c92977a8c33f0a79a0d3909415a0d7f596ae
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-04-01 15:32:47 -0700

    add customer route

--- src/resources/metrics/routes.ts
+++ src/resources/metrics/routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
+import { customerMetricsRouter } from "./customer/routes";
 import { financialMetricsRouter } from "./financial/routes";
 import { productMetricsRouter } from "./product/routes";
 import { repairMetricsRouter } from "./repair/routes";
 
 const metricsRouter = Router();
 
+metricsRouter.use("/customer", customerMetricsRouter);
+metricsRouter.use("/financial", financialMetricsRouter);
 metricsRouter.use("/product", productMetricsRouter);
 metricsRouter.use("/repair", repairMetricsRouter);
-metricsRouter.use("/financial", financialMetricsRouter);
 
 export { metricsRouter };
