db8d5fc246ec651b97733669625340fc752bf510
Athma Chidambaravasi <athma@Athmas-Mac-mini.local>
2025-05-01 09:49:15 -0700

    add middleware: filePayloadExists

---
+++ src/middlewares/filePayloadExists.ts
@@ -0,0 +1,29 @@
+import type { NextFunction, Request, Response } from "express";
+import type { FileUploadObject } from "../types";
+import createHttpError from "http-errors";
+
+function filesPayloadExistsMiddleware(
+    request: Request,
+    _response: Response,
+    next: NextFunction,
+) {
+    const files = request.files as
+        | FileUploadObject
+        | FileUploadObject[]
+        | undefined;
+
+    if (!files || (Array.isArray(files) && files.length === 0)) {
+        return next(
+            new createHttpError.BadRequest("No files found in request object"),
+        );
+    }
+
+    console.log("\n");
+    console.group("filesPayloadExistsMiddleware");
+    console.log({ files });
+    console.groupEnd();
+
+    return next();
+}
+
+export { filesPayloadExistsMiddleware };
